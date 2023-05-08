5544c726102de21edd04668902310229a0d78fb9
Drew Bidmead <dmbidmead@gmail.com>
2023-05-08 13:34:15 -0400

    JS functionality complete in desktop styling, including ensuring only one question is shown at a time. CSS animations implemented. CSS updated to prevent pointer events on the child span of the question div element.

---
+++ app.js
@@ -0,0 +1,47 @@
+const questionOne = document.querySelector('#one');
+const questionTwo = document.querySelector('#two');
+const questionThree = document.querySelector('#three');
+const questionFour = document.querySelector('#four');
+const questionFive = document.querySelector('#five');
+
+const questions = [questionOne, questionTwo, questionThree, questionFour, questionFive];
+
+let answerVisible = false;
+
+function toggle(question) {
+    let answer = question.parentElement.lastElementChild;
+    if(!question.classList.contains('active')) {
+        question.classList.add('active');
+        answer.classList.add('visible');
+        answer.setAttribute('style', 'max-height:300px; opacity:1; margin-top:1.3em;');
+        answerVisible = true;
+    } else if(question.classList.contains('active')) {
+        question.classList.remove('active');
+        answer.classList.remove('visible');
+        answer.setAttribute('style', 'max-height:0; opacity:0; margin-top:0;');
+        answerVisible = false;
+    };
+}
+
+questions.forEach((question) => {
+    question.addEventListener('click', (e) => {
+        let clickedQuestion = e.target;
+
+        if(answerVisible) {
+            let activeQuestionArr = questions.filter(question => !!question.classList.contains('active'));
+            let activeQuestion = activeQuestionArr[0];
+            if(activeQuestion != clickedQuestion) {
+                toggle(activeQuestion);
+                // don't return so that the clicked question will subsequently be toggled
+            } else {
+                toggle(clickedQuestion);
+                return;
+            }
+        }
+        
+        if(!answerVisible) { 
+            toggle(clickedQuestion);
+            return;
+        }
+    })
+})
